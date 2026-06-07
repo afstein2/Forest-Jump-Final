78cdc62281640552f978fc0a29330889741b60b1
Allie Stein <afstein@ucsc.edu>
2026-06-07 16:33:13 -0700

    added more tiles to cave level

--- assets/roguelikeDungeon_transparent.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="roguelikeDungeon_transparent" tilewidth="18" tileheight="18" tilecount="432" columns="27">
- <grid orientation="orthogonal" width="10" height="10"/>
- <image source="roguelikeDungeon_transparent.png" width="492" height="305"/>
-</tileset>
